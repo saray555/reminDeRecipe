e8482c4ca67343482d2ad8f3209b4915966473df
Noriko Ohnuki <nnorky@gmail.com>
2017-03-06 13:48:26 +0000

    added language.. but something is not right

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+  
+  get 'users/new'
 
+  get 'sessions/new'
+ 
+  resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
