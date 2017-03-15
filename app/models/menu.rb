7b5d4b2b85befcabdc87f4f35a314f268daf20ee
Noriko Ohnuki <nnorky@gmail.com>
2017-03-15 07:37:09 +0000

    added menu on controller

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,7 @@
 class Menu < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :menu_id, presence: true
+  validates :menu_name, presence: true
+  validates :description, presence: true, length: { maximum: 200 }
 end
